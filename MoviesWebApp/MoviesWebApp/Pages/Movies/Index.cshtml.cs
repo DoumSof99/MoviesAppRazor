c5596ed992d43c801be50773611f490eae490b30
DoumSof99 <sofiadmn99@gmail.com>
2023-03-26 22:33:23 +0300

    Add model

---
+++ MoviesWebApp/MoviesWebApp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MoviesWebApp.Data;
+using MoviesWebApp.Models;
+
+namespace MoviesWebApp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MoviesWebApp.Data.MoviesWebAppContext _context;
+
+        public IndexModel(MoviesWebApp.Data.MoviesWebAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
